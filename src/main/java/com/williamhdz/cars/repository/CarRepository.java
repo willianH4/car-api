63c859d3814d4a17feb020b10bb1dfb7d4220c19
Willian <williamhernandez450@gmail.com>
2023-08-18 04:43:16 -0600

    Add dependency Spring Data Rest and class DataSourceConfiguration

--- src/main/java/com/williamhdz/cars/repository/CarRepository.java
+++ src/main/java/com/williamhdz/cars/repository/CarRepository.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
 import com.williamhdz.cars.domain.Car;
 
+@RepositoryRestResource(path="vehicles")
 @Component
 public interface CarRepository extends JpaRepository<Car, Long> {
 
